d89aaf11888604f223ee1d386f326fde8db810c0
Mudabir <mudabirahmad1994@gmail.com>
2018-05-19 11:08:35 -0700

    made changes in setting.py

--- djangotest7/settings.py
+++ djangotest7/settings.py
@@ -74,9 +74,28 @@ WSGI_APPLICATION = 'djangotest7.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '',
+            'USER': 'root',
+            'PASSWORD': 'Opensesame1',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
 }
 
@@ -118,3 +137,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
